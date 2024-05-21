1ebd4dbfd8018bb014befd62300ee267a6e9dd80
knievel01 <romanhessel@gmail.com>
2024-05-21 20:33:52 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,80 +1,28 @@
+import streamlit as st
+import pandas as pd
 
+# Initialisiere einen leeren DataFrame zum Speichern der Daten
+data = []
 
-def main():
-    import streamlit as st
-    import pandas as pd
-    from dotenv import load_dotenv
+st.title("🎉 Anmeldung zum Hochzeitstag 🎉")
+st.caption("🚀 Um uns die Planung etwas zu vereinfachen sagt bitte kurz Bescheid ob Ihr kommt oder nicht.")
+st.caption("🤖 Wir freuen uns auf Euch! 🥳")
 
-    load_dotenv()
-    st.set_page_config(
-        page_title="Hochzeitstag Denise & Roman",
-        page_icon="💍",
-        layout="centered",
-        initial_sidebar_state="expanded"
-    )
- 
-    st.header("🎉 10 Jahre - Denise und Roman 🎉")
-    st.caption("Um uns die Planung zu vereinfachen, sagt bitte kurz Bescheid, ob ihr kommt und zu wie vielen Personen.")
-    st.caption("Wir freuen uns auf euch! 🎉")
-    # tanz emoji
-    st.caption("💃🕺")
-    with st.form("registration_form"):
-        st.caption("📝 Bitte füllen Sie das Formular aus und klicken Sie auf 'Anmeldung abschicken'")
-        st.title("Anmeldung")
-        name = st.text_input("Name")
-        confirmation = st.toggle("Wir kommen", ["Ja", "Nein"])
-        # Füge ein zu wie vielen Personen du kommst
-        people = st.select_slider(
-            "Anzahl Personen",
-             options=["1", "2", "3", "4", "5", "6"])
+# Formular erstellen
+with st.form(key='my_form'):
+    name = st.text_input(label='Name')
+    confirmation = st.radio(label='Ich bin dabei', options=['Ja', 'Nein'])
+    guests = st.number_input(label='Anzahl der Gäste', min_value=1, max_value=10)
+    submit_button = st.form_submit_button(label='Abschicken')
+
+    if submit_button:
+        data.append([name, confirmation, guests])  # Füge die Daten zu einer Liste hinzu
+        df = pd.DataFrame(data, columns=['Name', 'Confirmation', 'Amount of Guests'])
+        # bitte hänge die daten an die datei guests.csv an
+        df.to_csv('guests.csv', mode='a', index=False, header=False)
         
-        submit_button = st.form_submit_button("Abschicken")
-        
-        json_data = []
-        #### Abschicken
-        if submit_button:
-            if name and confirmation and people:
-                json_data = {
-                    "name": name,
-                    "confirmation": confirmation,
-                    "people": people
-                }
-                
-                mongo_write = write_to_mongodb(json_data)
-
-                if mongo_write:
-                    #send_email(json_data)   
-                    st.success("Danke fürs Bescheid sagen, " + name + ". Wir freuen uns auf euch!")
-                    st.balloons()
-                else:
-                    st.error("Fehler beim Speichern der Daten. Bitte versuch es später noch einmal.")
-            else:
-                st.error("Bitte füllen Sie die Pflichtfelder aus.")
-
-def write_to_mongodb(json_data):
-    # write json_data to mongodb atlas
-    import pymongo
-    CONNECTION_STRING = "mongodb+srv://XXXX:XXXX@cluster0.ty0skaq.mongodb.net/Cluster0"
-    # connect to MongoDB
-    print("opening connection to MongoDB")
-    client = pymongo.MongoClient(CONNECTION_STRING)
-    db = client["iok-triathlon-verl"]
-    collection = db["registrations"]
-    # insert data
-    print("inserting data")  
-    collection.insert_one(json_data)
-    # check if data was inserted
-    if collection.find_one(json_data):
-        print("Data was inserted")
-    else:
-        st.write("Data was not inserted")
-
-    # close the connection
-    client.close()
-    return True
-
-            
-
-
-if __name__ == "__main__":
-    main()
+        if confirmation == 'Ja':
+            st.success("Vielen Dank " + name + " für deine Anmeldung. Wir freuen uns!")
+            st.balloons()
+        else:   
+            st.error("Schade, dass du nicht kommen kannst. Vielleicht klappt es ja beim nächsten Mal.")
